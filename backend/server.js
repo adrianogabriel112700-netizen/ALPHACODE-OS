3e9fa6c445bda4dac052360ae6b9fb663cc2090e
Adriano Gabriel <adrianogabriel112700@gmail.com>
2026-09-02 17:56:04 -0300

    Conectando frontend com o backend em producao no Render

--- backend/server.js
+++ backend/server.js
@@ -34,3 +34,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`[ALPHA CODE SYSTEM] Servidor rodando na porta ${PORT}`);
 });
+
+
+ const API_URL = 'https://alphacode-backend.onrender.com'; 
